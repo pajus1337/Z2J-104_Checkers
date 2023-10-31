41d288e0289d5b924eb102ceff49829d8afea895
PaJuS <pajus1337@gmail.com>
2023-10-31 08:28:10 +0100

    Add project files.

---
+++ Z2J-104_Checkers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Z2J_104_Checkers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
