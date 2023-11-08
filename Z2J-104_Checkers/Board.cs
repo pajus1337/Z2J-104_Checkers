d6704a4885f41fd1d6544479c77a6fb0571b1456
pajusco <pajus1337@gmail.com>
2023-11-08 22:46:50 +0100

    Project update : - Added Board and Pawn class

---
+++ Z2J-104_Checkers/Board.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z2J_104_Checkers
+{
+    internal class Board
+    {
+    }
+}
